63a081aa76443d673f95145d08a25460a7f4974f
Hércules Ramlow <63937242+HRsniper@users.noreply.github.com>
2022-05-04 23:20:09 -0400

    init server

---
+++ server/src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+const PORT = process.env.PORT || 3333;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    server: "running"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("HTTP server running!");
+});
